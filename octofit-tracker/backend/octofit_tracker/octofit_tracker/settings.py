8da84473ecc41a2ae60cdea11da921b3b622a96f
Jaimegf10 <119013464+Jaimegf10@users.noreply.github.com>
2026-03-14 14:01:12 +0000

    changes2_2

--- octofit-tracker/backend/octofit_tracker/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/octofit_tracker/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'djongo',
+    'djongo', # MongoDB connector
     'corsheaders',
     'octofit_tracker',
 ]
